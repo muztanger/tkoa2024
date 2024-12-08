f73eeb55dab39d244499dee93e0da23e0283ee44
Johan Östrand <muztanger@gmail.com>
2024-12-08 19:57:51 +0100

    Quest1 green tests

--- Quest01.cs
+++ Quest01.cs
@@ -9,20 +9,20 @@ public sealed class Quest01
     public void Part1()
     {
         var baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
-        var fileName = Path.Combine(baseDir, "Input", $"Quest01.input");
+        var fileName = Path.Combine(baseDir, "Input", $"Quest01_01.input");
         Assert.IsTrue(Path.Exists(fileName));
         var lines = File.ReadAllLines(fileName);
         Assert.AreEqual(1, lines.Length);
         var index = "ABC";
         var points = new[] { 0, 1, 3 };
-        Assert.AreEqual(0, lines[0].Sum(c => points[index.IndexOf(c)]));
+        Assert.AreEqual(1339, lines[0].Sum(c => points[index.IndexOf(c)]));
     }
 
     [TestMethod]
     public void Part2()
     {
         var baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
-        var fileName = Path.Combine(baseDir, "Input", $"Quest01Part2.input");
+        var fileName = Path.Combine(baseDir, "Input", $"Quest01_02.input");
         Assert.IsTrue(Path.Exists(fileName));
         var lines = File.ReadAllLines(fileName);
         Assert.AreEqual(1, lines.Length);
@@ -45,16 +45,17 @@ public sealed class Quest01
             }
 
         }
-        Assert.AreEqual(0, sum);
+        Assert.AreEqual(5664, sum);
     }
 
     [TestMethod]
     public void Part3()
     {
         var baseDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..");
-        var fileName = Path.Combine(baseDir, "Input", $"Quest01Part3.input");
+        var fileName = Path.Combine(baseDir, "Input", $"Quest01_03.input");
         Assert.IsTrue(Path.Exists(fileName));
         var lines = File.ReadAllLines(fileName);
+
         Assert.AreEqual(1, lines.Length);
         var index = "ABCD";
         var points = new[] { 0, 1, 3, 5 };
@@ -63,20 +64,7 @@ public sealed class Quest01
         {
             if (chunk.Contains('x'))
             {
-                var other = chunk.Where(c => c != 'x').ToArray();
-                //if (chunk[1] == 'x')
-                //{
-                //    if (chunk[0] != 'x')
-                //    {
-                //        sum += points[index.IndexOf(chunk[0])];
-                //    }
-                    
-                //    if (chunk[2] != 'x')
-                //    {
-                //        sum += points[index.IndexOf(chunk[2])];
-                //    }
-                //}
-                //else 
+                var other = chunk.Where(c => c != 'x').ToArray(); 
                 if (other.Count() == 1)
                 {
                     sum += points[index.IndexOf(other[0])];
@@ -92,7 +80,6 @@ public sealed class Quest01
             }
 
         }
-        // less than 7xxx and more than 5xxx
-        Assert.AreEqual(0, sum);
+        Assert.AreEqual(27814, sum);
     }
 }
